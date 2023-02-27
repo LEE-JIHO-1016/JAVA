ad38bd812cdc162c79792a32277175282609fbed
LEE-JIHO-1016 <76989907+LEE-JIHO-1016@users.noreply.github.com>
2023-02-27 17:06:35 +0900

    Delete 이지호_L1_P4.java

--- 이지호_L1_P4.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-class Main {
-  public static void main(String[] args) {
-    int first = Integer.parseInt((new Scanner(System.in)).nextLine());
-    int second = Integer.parseInt((new Scanner(System.in)).nextLine());
-    System.out.println(first*second);
-  }
-}
