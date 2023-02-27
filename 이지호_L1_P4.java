0cf526b5b91968452ce62b40dca2ffb60771d25e
LEE-JIHO-1016 <76989907+LEE-JIHO-1016@users.noreply.github.com>
2023-02-27 16:57:08 +0900

    Add files via upload

---
+++ 이지호_L1_P4.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    int first = Integer.parseInt((new Scanner(System.in)).nextLine());
+    int second = Integer.parseInt((new Scanner(System.in)).nextLine());
+    System.out.println(first*second);
+  }
+}
